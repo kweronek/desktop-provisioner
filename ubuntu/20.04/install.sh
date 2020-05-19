673954e2fc097aefd1c7d79adbe4e01634dedd29
weronek <KWeronek@Fb2.FRA-UAS.de>
2020-05-19 16:02:32 +0200

    first commit

--- ubuntu/20.04/install.sh
+++ ubuntu/20.04/install.sh
@@ -1,8 +1,15 @@
 sudo -s
 apt update
-apt -install make
-apt install build-essential
 
+## insstall make
+apt -install make
+make -v
+
+## install
+apt install build-essential
+gcc -v
+
+## install go 1.13
 wget https://dl.google.com/go/go1.13.linux-amd64.tar.gz
 sha256sum go1.13.linux-amd64.tar.gz
 tar -C /usr/local -xzf go1.13.linux-amd64.tar.gz
@@ -10,30 +17,21 @@ export PATH=$PATH:/usr/local/go/bin
 mkdir go
 mkdir go/{src,bin,pkg}
 export $GOPATH=$HOME/go
+go version
 
 apt-get update -y
 apt-get install -y libseccomp-dev
 apt-get install -y pkg-config
 
+## install Docker
 sudo systemctl enable --now docker
 sudo usermod -aG docker root
 sudo usermod -aG docker ubuntu
+docker -v
 
 
-git init
-git clone https://github.com/rancher/k3c
-cd k3c
-make build
-make package
 
 
-docker pull rancher/k3s
-
-# docker exec -it k3s K3C_ADDRESS=/run/k3s/containerd/containerd.sock bash
-# docker exec -it K3C_ADDRESS=/run/k3s/containerd/containerd.sock k3s bash
-docker exec -it -e K3C_ADDRESS=/run/k3s/containerd/containerd.sock k3s --s
-
-./bin/k3c daemon --cni/bin=/bin --bootstrap-image=docker.io/rancher/k3c:dev
 
 
 	
