9c8840ad74014e73a369da8deec61850fe54b776
Karsten Weronek <kweronek@fb2.fra-uas.de>
2021-03-04 11:36:19 +0100

    Update desktop-install.sh

--- ubuntu/20.04/desktop-install.sh
+++ ubuntu/20.04/desktop-install.sh
@@ -1,6 +1,8 @@
 # install ubuntu desktop on premise
 #
 sudo apt update
+sudo apt-get update
+#
 sudo apt install -y git
 sudo apt install -y curl
 sudo apt install -y binutils
@@ -16,11 +18,12 @@ echo "export GOPATH=~/go" >> $HOME/.profile
 #
 sudo apt install -y openjdk-14-jdk
 sudo snap install protobuf --classic
-#
+
+# install OS-Virtualizer
 sudo snap install multipass
 sudo snap install docker
-#
-# install k3d
+
+# install rancher k3d
 wget -q -O - https://raw.githubusercontent.com/rancher/k3d/main/install.sh | bash
 #
 # install rancher k3c
@@ -30,20 +33,32 @@ make build"https://s3.amazonaws.com/gitlab-runner-downloads/master/deb/gitlab-ru
 ./bin/k3c daemon --group=$(id -g) &
 cd ..
 
-# install k3s
+# install rancher k3s
 curl -sfL https://get.k3s.io | sh -
+
+# install Terraform
+cd Downloads
+TER_VER=`curl -s https://api.github.com/repos/hashicorp/terraform/releases/latest | grep tag_name | cut -d: -f2 | tr -d \"\,\v | awk '{$1=$1};1'`
+wget https://releases.hashicorp.com/terraform/${TER_VER}/terraform_${TER_VER}_linux_amd64.zip
+unzip terraform_${TER_VER}_linux_amd64.zip
+sudo mv terraform /usr/local/bin/
+cd ..
+
+# install HELM
+sudo snap install helm --classic
+sudo snap install kubectl --classic
 
 sudo snap install tree
 sudo snap install htop
-sudo snap install kubectl --classic
 sudo snap install kate
-sudo snap install --classic code
+sudo snap install code -classic
 sudo snap install goland --classic
 sudo snap install pycharm-educational --classic
 sudo snap install arduino
 sudo usermod -a -G  dialout $USER
 sudo snap install postman
 
+# install Gitlab-runner
 cd ~/Downloads
 ARCH="amd64"
 curl -LJO "https://s3.amazonaws.com/gitlab-runner-downloads/master/rpm/gitlab-runner_${ARCH}.rpm"
@@ -62,23 +77,22 @@ echo "set nocompatible" > $HOME/.vimrc
 echo "export PATH=$PATH:." >> $HOME/.profile
 source $HOME/.profile
 #
-# Zoom-Client
+# install Zoom-Client
 cd ~/Downloads
 wget https://zoom.us/client/latest/zoom_amd64.deb
 sudo apt install ./zoom_amd64.deb
 cd ~
 #
-# Teams-Client
+# instll Teams-Client
 cd ~/Downloads
 wget https://packages.microsoft.com/repos/ms-teams/pool/main/t/teams/teams_1.3.00.16851_amd64.deb
 sudo dpkg -i teams_1.3.00.16851_amd64.deb
 cd ~
 #
-# VPN-Client FRA-UAS
+# install VPN-Client FRA-UAS fortinet
 wget -O - https://repo.fortinet.com/repo/6.4/ubuntu/DEB-GPG-KEY | sudo apt-key add -
 sudo chmod 777 /etc/apt/sources.list
 echo "deb [arch=amd64] https://repo.fortinet.com/repo/6.4/ubuntu/ /bionic multiverse" >> /etc/apt/sources.list
 sudo chmod 644 /etc/apt/sources.list
-sudo apt-get update
 sudo apt install forticlient
 #
